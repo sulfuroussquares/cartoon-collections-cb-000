27910dc529dff3ca88f6bae36a6e4f5a7cf81a00
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-07 17:36:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(names)
-  
+names.each_with_index {|name, index| puts "#{index}. #{name}"}
 end
 
 def summon_captain_planet# code an argument here
@@ -14,3 +14,8 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+a = [11,22,31,224,44].each.with_index { |val,index| puts "index: #{index} for #{val}" if val < 30}
+index: 0 for 11
+index: 1 for 22
+=> [11, 22, 31, 224, 44]
